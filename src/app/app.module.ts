30667242db7d20e38e5e89da1385b10a90d8e9e8
Mathias Fuchs <fuchsmath@gmail.com>
2018-10-27 18:52:56 +0200

    added sound. Up for debate.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,9 @@ import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 import {RoundProgressModule} from 'angular-svg-round-progressbar';
-
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+
 
 @NgModule({
   declarations: [
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import {Ion, NavController} from 'ionic-angular';
 import { primeNbrContainer} from './primeNbrContainer';
-import {RoundProgressEase} from 'round-progress.ease';
 import { Observable } from 'rxjs/Observable'
 import 'rxjs/add/observable/timer'
 import 'rxjs/add/operator/map'
 import 'rxjs/add/operator/take'
-import { Pipe, PipeTransform } from '@angular/core';
+import {Howl, Howler} from 'howler';
 
 
 @Component({
@@ -15,8 +14,9 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class HomePage {
 
+  // parts needed for the countdown timer
   current: number = 5;
-  max: number = 5;
+  availableAnswerTime: number = 5;
   stroke: number = 15;
   radius: number = 125;
   semicircle: boolean = false;
@@ -25,19 +25,19 @@ export class HomePage {
   clockwise: boolean = true;
   color: string = '#ce1609';
   background: string = '#eaeaea';
-  duration: number = 800;
+  duration: number = 1000;
 
-
+  changeColor: boolean = false;
   calculation: string = "2017-17";
   subtractors: number[] = [7,13];
   randomBigPrimeNbr: number = 15;
   bigPrimeNumbers: number[] = primeNbrContainer.getBigPrimeNbrs();
-  input: number = 0;
+  input: String = "";
   subtractor: number = 0;
 
   countDown;
-  counter = 5;
   tick = 1000;
+  counter = 5;
 
 
   getRandomNbr(list: number[]) {
@@ -48,31 +48,28 @@ export class HomePage {
     this.randomBigPrimeNbr = this.getRandomNbr(this.bigPrimeNumbers);
     this.subtractor = this.getRandomNbr(this.subtractors);
     this.updateCalculationDisplay(this.randomBigPrimeNbr, this.subtractor);
+    this.input = "";
+
   }
 
 
 
   updateCalc() {
-    if ( parseInt(this.input) == this.randomBigPrimeNbr - this.subtractor) {
+    let wasInputRight: boolean;
+    wasInputRight =  (parseInt(this.input) == (this.randomBigPrimeNbr - this.subtractor));
+    if ( wasInputRight) {
       this.randomBigPrimeNbr = this.randomBigPrimeNbr - this.subtractor;
       this.subtractor = this.getRandomNbr(this.subtractors);
       this.updateCalculationDisplay(this.randomBigPrimeNbr, this.subtractor)
+      // Decrease available answerTime
+      -- this.availableAnswerTime;
+    } else {
+      this.wrongAnswerRoutine();
+
     }
-    this.input = null;
 
-    this.countDown = Observable.timer(0, this.tick)
-      .take(this.counter)
-      .map(() => {
-      this.current = this.counter;
-        return --this.counter;
-      }
-    )
+    this.counterRoutine();
 
-  }
-
-
-  refresh()
-  {
   }
 
   private updateCalculationDisplay(randomBigPrimeNbr: number, subtractor: number) {
@@ -80,4 +77,43 @@ export class HomePage {
 
   }
 
+  private wrongAnswerRoutine() {
+    let sound = new Howl({
+      src: ['http://localhost:8100/assets/wrong.mp3']
+    });
+
+    sound.play();
+
+
+    // increase available answer time and restart
+    this.availableAnswerTime = this.availableAnswerTime +3;
+    this.randomBigPrimeNbr = this.getRandomNbr(this.bigPrimeNumbers);
+    this.updateCalculationDisplay(this.randomBigPrimeNbr, this.subtractor)
+    this.counter = this.availableAnswerTime;
+    this.changeColor=true;
+    this.counterRoutine();
+
+  }
+
+
+
+  private counterRoutine() {
+    this.counter = this.availableAnswerTime;
+    this.input = "";
+    this.countDown = Observable.timer(0, this.tick)
+      .take(this.counter)
+      .map(() => {
+          --this.counter;
+          if ( this.counter <= this.availableAnswerTime -2 ) {
+            this.changeColor=false;
+
+          }
+          if ( this.counter <= 0) {
+            // timer is over trigger answer false routine
+            return this.wrongAnswerRoutine();
+          }
+          return  this.current = this.counter;
+        }
+      )
+  }
 }
